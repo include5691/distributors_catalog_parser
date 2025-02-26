e765aa938529d06d66c55e60654549a78a67a7e9
Andrey Pshenitsyn <include5691@gmail.com>
2025-02-26 12:03:41 +0500

    update[parser/parse]: add subdirectory existing check in parse_category func

--- parser/parse.py
+++ parser/parse.py
@@ -5,24 +5,27 @@ from .categories import get_subcategories_urls
 from .products import get_products
 from .write import write_products_csv
 
+def _get_dir_path(url: str) -> pathlib.Path:
+    parsed_url = urlparse(url)
+    dir_path = pathlib.Path(__file__).parent.parent / "csv_files" / parsed_url.path.split("-")[-1]
+
 def parse_caregory(url: str):
     subcategories = get_subcategories_urls(url)
     if not subcategories:
         logging.error(f"Subcategories not found for url {url}")
         return None
     for subcategory in subcategories:
-        parse_subcategory(subcategory)
+        dir_path = _get_dir_path(subcategory)
+        if not dir_path.exists():
+            parse_subcategory(subcategory)
 
 def parse_subcategory(url: str, categories_path: str | None = None):
     products = get_products(url, categories_path)
     if not products:
         logging.error(f"Products not found for url {url}")
         return None
-    parsed_url = urlparse(url)
-    dir_path = pathlib.Path(__file__).parent.parent / "csv_files" / parsed_url.path.split("-")[-1]
+    dir_path = _get_dir_path(url)
     if not dir_path.exists():
         dir_path.mkdir(parents=True)
     for n in range(len(products) // 15 + 1):
-        print(dir_path)
-        print(str(dir_path) + f"{parsed_url.path}_{n}.csv")
-        write_products_csv(products[n*15:(n+1)*15], file_name=str(dir_path) + f"{parsed_url.path}_{n}.csv")
+        write_products_csv(products[n*15:(n+1)*15], file_name=str(dir_path) + f"{dir_path.name}_{n}.csv")
